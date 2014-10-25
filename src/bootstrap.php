c1c2ea68c9e5b8dfdfe1a417e4e9eadedb2f4bf7
Qas Ashraf <mail@qasashraf.co.uk>
2014-10-25 19:00:04 +0100

    Use configserviceprovider to detect dev/prod. Add Doctrine settings which read from config settings.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,4 +2,28 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app->register(new Whoops\Provider\Silex\WhoopsServiceProvider);
+
+// Figure out environment based upon APP_ENV variable (environment)
+$env = getenv('APP_ENV') ? : 'prod';
+
+// Read configuration based upon environment
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . "/../config/$env.json"));
+$app['debug'] = $app['config']['debug'];
+
+// Database.
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+      'driver'   => 'pdo_mysql',
+      'path'     => __DIR__.'/app.db',
+      'dbname'   => $app['config']['database']['database'],
+      'host'     => $app['config']['database']['server'],
+      'user'     => $app['config']['database']['username'],
+      'password' => $app['config']['database']['password'],
+      'port'     => $app['config']['database']['port'],
+    ),
+  ));
+
+// On dev and stage...
+if ($app['debug']) {
+    $app->register(new Whoops\Provider\Silex\WhoopsServiceProvider);
+}

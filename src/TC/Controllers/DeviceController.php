20212217407169c6aebac9cad9e60a759fc88d01
Qas Ashraf <mail@qasashraf.co.uk>
2014-10-26 09:00:57 +0000

    Revert "Made logon call hit Shane's code which starts the grouping of locations. As soon as someone logs in, they appear in the Locations."
    This reverts commit f1ac29a9926c7f1f80c902286297f45c42dff935.

--- src/TC/Controllers/DeviceController.php
+++ src/TC/Controllers/DeviceController.php
@@ -72,10 +72,6 @@ class DeviceController{
         );
 
         $this->db->insert('device', $data);
-
-        // Call Shane's magic update
-        $this->updateDeviceLocation($key, $data['lat'], $data['lon']);
-
         return $key;
     }
 
